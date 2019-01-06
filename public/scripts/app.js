5163abab9c074797801d6f2241b05e62e1b81044
ryanboris <ryan.boris@gmail.com>
2019-01-06 14:00:56 -0500

    adds babel support for post es5 and react

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,20 @@
-console.log('running.')
+'use strict';
 
-var template = React.createElement('h1', { id: 'someId' }, 'Something new!')
+var App = function App() {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Hi!'
+    ),
+    React.createElement(
+      'h3',
+      null,
+      'How are ya?!'
+    )
+  );
+};
 
-ReactDOM.render(template, document.getElementById('root'))
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Hi!</h1>
+      <h3>How are ya?!</h3>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
