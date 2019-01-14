9092ef388713e269f7e2621ae60830f32b0565d3
ryanboris <ryan.boris@gmail.com>
2019-01-14 05:02:25 -0500

    adds decision button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,55 +1,89 @@
-'use strict'
+'use strict';
 
+/* eslint-disable */
 var app = {
   title: 'Indecision',
   subtitle: 'fun',
   options: ['one', 'two']
-}
+};
 
 var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault()
-  var option = e.target.elements.option.value
+  e.preventDefault();
+  var option = e.target.elements.option.value;
   if (option) {
-    app.options.push(option)
-    e.target.elements.option.value = ''
-    render()
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
   }
-}
+};
 
 var removeAll = function removeAll() {
-  app.options = []
-  render()
-}
+  app.options = [];
+  render();
+};
+
+var onMakeDecision = function onMakeDecision() {
+  var length = app.options.length;
+
+  var randomNum = Math.floor(Math.random() * length);
+  var option = app.options[randomNum];
+  alert(option);
+};
 
 var render = function render() {
   var template = React.createElement(
     'div',
     null,
-    React.createElement('h1', null, app.title),
-    app.subtitle && React.createElement('p', null, app.subtitle),
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
     React.createElement(
       'p',
       null,
       app.options.length > 0 ? 'Here are your options' : 'none'
     ),
     React.createElement(
+      'button',
+      { disabled: app.options.length === 0, onClick: onMakeDecision },
+      'What Should I do?'
+    ),
+    React.createElement(
       'ol',
       null,
-      app.options.map(function(option) {
-        return React.createElement('li', { key: option }, option)
+      app.options.map(function (option) {
+        return React.createElement(
+          'li',
+          { key: option },
+          option
+        );
       })
     ),
-    React.createElement('button', { onClick: removeAll }, 'Remove All'),
+    React.createElement(
+      'button',
+      { onClick: removeAll },
+      'Remove All'
+    ),
     React.createElement(
       'form',
       { onSubmit: onFormSubmit },
       React.createElement('input', { type: 'text', name: 'option' }),
-      React.createElement('button', { type: 'submit' }, 'Add Option')
+      React.createElement(
+        'button',
+        { type: 'submit' },
+        'Add Option'
+      )
     )
-  )
+  );
 
-  var appRoot = document.getElementById('root')
-  ReactDOM.render(template, appRoot)
-}
+  var appRoot = document.getElementById('root');
+  ReactDOM.render(template, appRoot);
+};
 
-render()
+render();
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const app = {
   title: 'Indecision',
   subtitle: 'fun',
@@ -19,12 +20,22 @@ const removeAll = () => {
   render()
 }
 
+const onMakeDecision = () => {
+  const { length } = app.options
+  const randomNum = Math.floor(Math.random() * length)
+  const option = app.options[randomNum]
+  alert(option)
+}
+
 const render = () => {
   const template = (
     <div>
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options.length > 0 ? 'Here are your options' : 'none'}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>
+        What Should I do?
+      </button>
       <ol>
         {app.options.map(option => {
           return <li key={option}>{option}</li>
