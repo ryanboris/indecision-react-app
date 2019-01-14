f9e6cd407d3eb23784d6f5d88581f6fda55a1b61
ryanboris <ryan.boris@gmail.com>
2019-01-14 01:15:03 -0500

    adds counter example

---
+++ playground/count.js
@@ -0,0 +1,33 @@
+// let count = 0
+// const addOne = () => {
+//     count++
+//     renderCounterApp()
+// }
+// const subOne = () => {
+//   if(count !== 0) {
+//       count--
+//       renderCounterApp()
+//   } else {
+//       return null
+//   }
+// }
+// const reset = () => {
+//   count = 0
+//   renderCounterApp()
+// }
+
+// const renderCounterApp = () => {
+//     const templateTwo = (
+//         <div>
+//             <h1>
+//                 Count: { count }
+//             </h1>
+//             <button onClick={addOne}>+</button>
+//             <button onClick={subOne}>-</button>
+//             <button onClick={reset}>0</button>
+//         </div>
+//     )
+//     ReactDOM.render(templateTwo, document.getElementById('root'))
+// }
+
+// renderCounterApp()
